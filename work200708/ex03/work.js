28d19dae69ecf5d5aa215c69d9b3c5270cec35e8
yukgong <31286070+yukgong@users.noreply.github.com>
2020-07-08 20:20:36 +0900

    [add] ex03

---
+++ work200708/ex03/work.js
@@ -0,0 +1,34 @@
+const plus = document.querySelectorAll(".plus"),
+    minus = document.querySelector(".minus"),
+    multiple = document.querySelector(".multiple"),
+    division = document.querySelector(".division"),
+    mod = document.querySelector(".mod"),
+    answer = document.querySelector(".answer");
+
+
+
+function calcPlus(){
+
+}
+
+function calcMinus(){
+    
+}
+
+function calcMultiple(){
+    
+}
+
+function calcDivision(){
+    
+}
+
+function calcMod(){
+    
+}
+
+function init() {
+
+}
+
+init();
